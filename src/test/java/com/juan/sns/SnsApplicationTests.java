09ccceec920d28487a275c58c3194e9322305659
juan <crumbled23@gmail.com>
2023-12-23 16:38:55 +0900

    init : initailize project

---
+++ src/test/java/com/juan/sns/SnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
